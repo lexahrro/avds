72267164d3834e380d88511a3e41708145dfb651
DrMalou <louismalrieu@orange.fr>
2017-12-07 12:12:47 +0100

    commit

--- index.php
+++ index.php
@@ -25,6 +25,147 @@
 
 </section>
 
+
+
+
+
+
+
+
+
+
+
+<div class="container">
+
+    <div class="row">
+
+
+
+        <a class="title">Votre voiture d'occasion au meilleur prix</a>
+
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+<div class="imageonepage"></div>
+
+
+
+
+
+<div class="container-fluid titredeux">
+
+    <div class="row">
+
+        <a class=" col-md-3 titredeuxpolice btn btn-primary"> Dernières Annonces </a>
+
+
+
+
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+<div class="container-fluid annonces">
+
+    <div class="row">
+
+        <div class="col-md-4 premiere exempleimg">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/exemplevoiture.jpg" class='img-responsive'>
+
+        </div>
+
+        <div class=" col-md-4 deuxième exempleimg">
+
+        </div>
+
+        <div class=" col-md-4 troisième exempleimg">
+
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<div class="col-sm-4 inscription">
+    <div id="horizontal-flip" class="card inscription2">
+        <div class="flip">
+            <div class="front">
+                <h1>
+                    <span>Contact</span>
+                    <span class="big">Us Now</span>
+                </h1>
+            </div>
+            <div class="back">
+                <input placeholder="Name">
+                <input placeholder="Daytime Phone">
+                <input placeholder="Email Address">
+                <button>Send</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<script  src="js/index.js"></script>
+
+
+
+
+
+
+
+
+
+
+
 </div>
 </body>
 </html>
